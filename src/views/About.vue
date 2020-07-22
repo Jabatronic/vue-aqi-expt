b3b74a8fe948619f786f5d320860403d64d8042b
Jabulani Robbins <jabatronic@gmail.com>
2020-07-22 14:05:49 +0100

    add axios and test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Axios Test</h1>
+    <div
+      v-for="user in users"
+      :key="user.id"
+    >
+      <h2>{{ user.name }}</h2>
+      <p></p>
+    </div>
+    <button @click="fetchUsers">Fetch Data!</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    fetchUsers: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com/users'
+      this.$http.get(baseURI)
+        .then((result) => {
+          this.users = result.data
+        }).catch((err) => {
+          console.warn('there was a problem!: ' + err.response.log)
+        })
+    }
+  }
+}
+</script>
