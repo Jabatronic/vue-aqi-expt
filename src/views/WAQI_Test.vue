6266443eae86a96b83d3195e02a460513705f8da
Jabulani Robbins <jabatronic@gmail.com>
2020-08-01 16:20:44 +0100

    add location search field

--- src/views/WAQI_Test.vue
+++ src/views/WAQI_Test.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
     <h1>Get WAQI Data (Test)</h1>
-    <h2>{{ city }}</h2>
-    <h2>{{ aqi }}</h2>
-    <button @click="fetchUsers">Fetch Data!</button>
+    <input type="text" v-model="locationSearchInput" v-on:keyup.enter="submitSearch" />
+    <div v-if="isSearchSubmitted" class="message">
+      <h2 v-if="warning">{{ warning }}</h2>
+      <h2>{{ city }}</h2>
+      <h2>{{ aqi }}</h2>
+    </div>
+    <!-- <button @click="fetchUsers">Fetch Data!</button> -->
     <GenCircles :iaqiData="iaqi"/>
   </div>
 </template>
@@ -19,15 +23,22 @@ export default {
   data () {
     return {
       city: '',
+      locationSearchInput: '',
+      isSearchSubmitted: false,
       aqi: '',
-      iaqi: null
+      iaqi: null,
+      warning: ''
     }
   },
   methods: {
-    fetchUsers: function () {
+    submitSearch: function () {
+      this.isSearchSubmitted = true
+      this.fetchData()
+    },
+    fetchData: function () {
       const apiUrl = 'https://api.waqi.info/feed'
       const authToken = '19c6276b13bc059b2f5811c5037638346d286d15'
-      const station = 'Beijing'
+      const station = this.locationSearchInput
 
       const apiRequest = `${apiUrl}/${station}/?token=${authToken}`
 
@@ -36,7 +47,12 @@ export default {
           this.city = result.data.data.city.name
           this.aqi = result.data.data.aqi
           this.iaqi = result.data.data.iaqi
+          this.warning = ''
         }).catch((err) => {
+          this.aqi = ''
+          this.city = ''
+          this.iaqi = null
+          this.warning = 'there was a problem'
           console.warn('there was a problem!: ' + err.response.log)
         })
     }
