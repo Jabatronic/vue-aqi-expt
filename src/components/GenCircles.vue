e7749ade0b201efc6f54fd09e5c028a90722c17b
Jabulani Robbins <jabatronic@gmail.com>
2020-08-08 22:52:46 +0100

    refactor

--- src/components/GenCircles.vue
+++ src/components/GenCircles.vue
@@ -12,15 +12,15 @@
       >
         <g :id="'key_' + index">
           <circle
-            :fill="getColour(name, value.v)"
-            stroke="white"
-            stroke-width=".1"
+            fill="#fff"
+            :stroke="getColour(name, value.v)"
+            stroke-width="7"
             cx="0"
             cy="0"
             r="50"
           />
           <text
-            fill="white"
+            fill="#2c3e50"
             font-size="90%"
             text-anchor="middle"
             alignment-baseline="central"
@@ -28,7 +28,7 @@
             <tspan class="polutant-name">{{ name }}</tspan>
           </text>
           <text
-            fill="white"
+            fill="#2c3e50"
             font-size="90%"
             text-anchor="middle"
             alignment-baseline="central"
@@ -43,51 +43,7 @@
 </template>
 
 <script>
-/**
- * Define index boundaries for each polutant
- */
-const aqBoundaries = {
-  o3: {
-    low: [33, 66, 100],
-    moderate: [120, 140, 160],
-    high: [187, 213, 240],
-    v_high: [240]
-  },
-  no2: {
-    low: [67, 134, 200],
-    moderate: [267, 334, 400],
-    high: [467, 534, 600],
-    v_high: [601]
-  },
-  so2: {
-    low: [88, 177, 266],
-    moderate: [354, 443, 532],
-    high: [710, 887, 1064],
-    v_high: [1065]
-  },
-  pm25: {
-    low: [11, 23, 35],
-    moderate: [41, 47, 53],
-    high: [58, 64, 70],
-    v_high: [71]
-  },
-  pm10: {
-    low: [16, 33, 50],
-    moderate: [58, 66, 75],
-    high: [83, 91, 100],
-    v_high: [101]
-  }
-}
-
-/**
- * Define colours for each boundary
- */
-const boundaryColors = {
-  low: ['#73BE75', '#32FF00', '#33CE00'],
-  moderate: ['#FEFF00', '#FFCF02', '#FF9A00'],
-  high: ['#FF6463', '#FF0202', '#990002'],
-  v_high: ['#CF31FF']
-}
+import { aqBoundaries, colors } from '@/modules/constants'
 
 export default {
   name: 'GenCircles',
@@ -107,26 +63,16 @@ export default {
       }
       return big5obj
     },
-    getColour (name, value) {
-      if (name in aqBoundaries) {
-        let counter = 0
-        for (const boundary in aqBoundaries[name]) {
-          const boundaryIndex = aqBoundaries[name][boundary]
-          const boundaryMax = Math.max(...boundaryIndex)
-          if (value <= boundaryMax) {
-            // const position = boundaryIndex.indexOf(value)
-            // console.log(position)
-            // console.log(value)
-            // console.log(boundaryIndex)
-            // console.log('> ' + boundaryColors[boundary][counter])
-            return boundaryColors[boundary][counter]
-          } else if (value >= aqBoundaries[name].v_high) {
-            return boundaryColors.v_high
+    getColour (pollutantName, value) {
+      // console.log(aqBoundaries)
+      if (pollutantName in aqBoundaries) {
+        // console.log(aqBoundaries[pollutantName])
+        for (const pollutantRank in aqBoundaries[pollutantName]) {
+          const subRank = aqBoundaries[pollutantName][pollutantRank]
+          if (value >= subRank.min && value <= subRank.max) {
+            return colors[pollutantRank]
           }
-          counter++
         }
-      } else {
-        return false
       }
     }
   }
---
+++ src/modules/constants.js
@@ -0,0 +1,230 @@
+/**
+ * Define index boundaries for each polutant
+ */
+const aqBoundaries = {
+  o3: {
+    1: {
+      min: 0,
+      max: 33
+    },
+    2: {
+      min: 34,
+      max: 66
+    },
+    3: {
+      min: 67,
+      max: 100
+    },
+    4: {
+      min: 101,
+      max: 120
+    },
+    5: {
+      min: 121,
+      max: 140
+    },
+    6: {
+      min: 141,
+      max: 160
+    },
+    7: {
+      min: 161,
+      max: 187
+    },
+    8: {
+      min: 188,
+      max: 213
+    },
+    9: {
+      min: 214,
+      max: 240
+    },
+    10: {
+      min: 241,
+      max: Infinity
+    }
+  },
+  no2: {
+    1: {
+      min: 0,
+      max: 67
+    },
+    2: {
+      min: 68,
+      max: 134
+    },
+    3: {
+      min: 135,
+      max: 200
+    },
+    4: {
+      min: 201,
+      max: 267
+    },
+    5: {
+      min: 268,
+      max: 334
+    },
+    6: {
+      min: 335,
+      max: 400
+    },
+    7: {
+      min: 401,
+      max: 467
+    },
+    8: {
+      min: 468,
+      max: 534
+    },
+    9: {
+      min: 535,
+      max: 600
+    },
+    10: {
+      min: 601,
+      max: Infinity
+    }
+  },
+  so2: {
+    1: {
+      min: 0,
+      max: 88
+    },
+    2: {
+      min: 89,
+      max: 177
+    },
+    3: {
+      min: 178,
+      max: 266
+    },
+    4: {
+      min: 267,
+      max: 354
+    },
+    5: {
+      min: 355,
+      max: 443
+    },
+    6: {
+      min: 444,
+      max: 532
+    },
+    7: {
+      min: 533,
+      max: 710
+    },
+    8: {
+      min: 711,
+      max: 887
+    },
+    9: {
+      min: 888,
+      max: 1064
+    },
+    10: {
+      min: 1065,
+      max: Infinity
+    }
+  },
+  pm25: {
+    1: {
+      min: 0,
+      max: 11
+    },
+    2: {
+      min: 12,
+      max: 23
+    },
+    3: {
+      min: 24,
+      max: 35
+    },
+    4: {
+      min: 36,
+      max: 41
+    },
+    5: {
+      min: 42,
+      max: 47
+    },
+    6: {
+      min: 48,
+      max: 53
+    },
+    7: {
+      min: 54,
+      max: 58
+    },
+    8: {
+      min: 59,
+      max: 64
+    },
+    9: {
+      min: 65,
+      max: 70
+    },
+    10: {
+      min: 71,
+      max: Infinity
+    }
+  },
+  pm10: {
+    1: {
+      min: 0,
+      max: 16
+    },
+    2: {
+      min: 12,
+      max: 33
+    },
+    3: {
+      min: 24,
+      max: 50
+    },
+    4: {
+      min: 51,
+      max: 58
+    },
+    5: {
+      min: 59,
+      max: 66
+    },
+    6: {
+      min: 67,
+      max: 75
+    },
+    7: {
+      min: 76,
+      max: 83
+    },
+    8: {
+      min: 84,
+      max: 91
+    },
+    9: {
+      min: 92,
+      max: 100
+    },
+    10: {
+      min: 101,
+      max: Infinity
+    }
+  }
+}
+
+const colors = {
+  1: '#9CFF9C',
+  2: '#32FF00',
+  3: '#33CE00',
+  4: '#FEFF00',
+  5: '#FFCF02',
+  6: '#FF9A00',
+  7: '#FF6463',
+  8: '#FF0202',
+  9: '#990002',
+  10: '#CF31FF'
+}
+
+export { aqBoundaries, colors }
