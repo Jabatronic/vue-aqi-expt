61dc2c075a3e6ee7fb6f94ed2f0eabb5c4d07b70
Jabulani Robbins <jabatronic@gmail.com>
2020-08-12 14:19:26 +0100

    create sequencer

--- src/modules/genSounds.js
+++ src/modules/genSounds.js
@@ -1,14 +1,29 @@
-import { Synth } from 'tone'
+import { Synth, Sequence, Transport } from 'tone'
 
-const genSounds = (note) => {
+const genSounds = () => {
   // test init
   console.log('Sound generator initialised')
 
   // create a synth and connect it to the main output (your speakers)
   const synth = new Synth().toDestination()
 
-  // play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease(note, '1n')
+  // notes to be played
+  const notesToBePlayed = ['C4', 'Eb4', 'G4', 'Bb4']
+
+  // create a sequence
+  const synthPart = new Sequence(
+    function (time, note) {
+      synth.triggerAttackRelease(note, '10hz', time)
+    },
+    notesToBePlayed,
+    '4n'
+  )
+
+  // Set up synth ready to be played
+  synthPart.start()
+
+  // start transport (main timeline)
+  Transport.start()
 }
 
 export default (genSounds)
